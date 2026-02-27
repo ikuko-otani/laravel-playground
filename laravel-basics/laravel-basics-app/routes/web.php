989d8030475fc7e3366bca53021f2b63ec58c326
ikuko-otani <ikuko.otani.dev@gmail.com>
2026-02-28 06:36:03 +0900

    feat: add basic GET routes for about and contact

--- laravel-basics/laravel-basics-app/routes/web.php
+++ laravel-basics/laravel-basics-app/routes/web.php
@@ -2,10 +2,19 @@
 
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
 Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/greeting', function () {
   return 'Hello from Laravel routing study!';
 });
+
+Route::get('/about', function () {
+  return 'About page';
+});
+
+Route::get('/contact', function () {
+  return 'Contact page';
+});
+
